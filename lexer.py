f3481239d0c113036d65b028af0bea366308d91b
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-05-08 17:37:45 -0500

    :sparkles: Operators of one character

--- lexer.py
+++ lexer.py
@@ -18,6 +18,40 @@ class Lexer:
     def next_token(self) -> Token:
         if match(r'\=', self._character):
             token = Token(TokenType.TK_ASIGNACION, self._character, self._line, self._position + 1)
+        elif match(r'\,', self._character):
+            token = Token(TokenType.TK_COMA, self._character, self._line, self._position + 1)
+        elif match(r'\]', self._character):
+            token = Token(TokenType.TK_CORCHETE_DERECHO, self._character, self._line, self._position + 1)
+        elif match(r'\[', self._character):
+            token = Token(TokenType.TK_CORCHETE_IZQUIERDO, self._character, self._line, self._position + 1)
+        elif match(r'\/', self._character):
+            token = Token(TokenType.TK_DIVISION, self._character, self._line, self._position + 1)
+        elif match(r'\:', self._character):
+            token = Token(TokenType.TK_DOS_PUNTOS, self._character, self._line, self._position + 1)
+        elif match(r'\}', self._character):
+            token = Token(TokenType.TK_LLAVE_DERECHA, self._character, self._line, self._position + 1)
+        elif match(r'\{', self._character):
+            token = Token(TokenType.TK_LLAVE_IZQUIERDA, self._character, self._line, self._position + 1)
+        elif match(r'\>', self._character):
+            token = Token(TokenType.TK_MAYOR, self._character, self._line, self._position + 1)
+        elif match(r'\<', self._character):
+            token = Token(TokenType.TK_MENOR, self._character, self._line, self._position + 1)
+        elif match(r'\%', self._character):
+            token = Token(TokenType.TK_MODULO, self._character, self._line, self._position + 1)
+        elif match(r'\*', self._character):
+            token = Token(TokenType.TK_MULTIPLICACION, self._character, self._line, self._position + 1)
+        elif match(r'\)', self._character):
+            token = Token(TokenType.TK_PARENTESIS_DERECHO, self._character, self._line, self._position + 1)
+        elif match(r'\(', self._character):
+            token = Token(TokenType.TK_PARENTESIS_IZQUIERDO, self._character, self._line, self._position + 1)
+        elif match(r'\^', self._character):
+            token = Token(TokenType.TK_POTENCIACION, self._character, self._line, self._position + 1)
+        elif match(r'\.', self._character):
+            token = Token(TokenType.TK_PUNTO, self._character, self._line, self._position + 1)
+        elif match(r'\;', self._character):
+            token = Token(TokenType.TK_PUNTO_Y_COMA, self._character, self._line, self._position + 1)
+        elif match(r'\-', self._character):
+            token = Token(TokenType.TK_RESTA, self._character, self._line, self._position + 1)
         elif match(r'\+', self._character):
             token = Token(TokenType.TK_SUMA, self._character, self._line, self._position + 1)
         else:
