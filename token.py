6476ba59d78a66f564000ed386ba86e5b5a2aa30
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-05-08 21:35:26 -0500

    :zap: Added reserved words

--- token.py
+++ token.py
@@ -3,7 +3,10 @@ from enum import(
   Enum,
   unique
 )
-from typing import NamedTuple
+from typing import (
+  NamedTuple,
+  Dict,
+)
 
 @unique
 class TokenType(Enum):
@@ -17,7 +20,7 @@ class TokenType(Enum):
   TK_LLAVE_DERECHA = auto() # One '}'
   TK_LLAVE_IZQUIERDA = auto() # One '{'
   TK_MAYOR = auto() # One '>'
-  TK_MENOR = auto() # One '<'
+  TK_MENOR = auto() # One '<'W
   TK_MODULO = auto() # One '%'
   TK_MULTIPLICACION = auto() # One '*'
   TK_PARENTESIS_DERECHO = auto() # One ')'
@@ -130,3 +133,91 @@ class Token(NamedTuple):
       return f'<{self.token_type.name},{self.literal},{self.lineno},{self.position}>'
     else:
       return f'<{self.token_type.name.lower()},{self.literal},{self.lineno},{self.position}>'
+
+def lookup_token_type(literal: str) -> TokenType:
+  keywords: Dict[str, TokenType] = {
+
+    # Reserved
+    'and': TokenType.AND,
+    'constantes': TokenType.CONSTANTES,
+    'hasta': TokenType.HASTA,
+    'matriz': TokenType.MATRIZ,
+    'paso': TokenType.PASO,
+    'registro': TokenType.REGISTRO,
+    'sino': TokenType.SINO,
+    'vector': TokenType.VECTOR,
+    'archivo': TokenType.ARCHIVO,
+    'desde': TokenType.DESDE,
+    'inicio': TokenType.INICIO,
+    'mientras': TokenType.MIENTRAS,
+    'subrutina': TokenType.SUBRUTINA,
+    'repetir': TokenType.REPETIR,
+    'tipos': TokenType.TIPOS,
+    'caso': TokenType.CASO,
+    'eval': TokenType.EVAL,
+    'lib': TokenType.LIB,
+    'not': TokenType.NOT,
+    'programa': TokenType.PROGRAMA,
+    'retorna': TokenType.RETORNA,
+    'var': TokenType.VAR,
+    'const': TokenType.CONST,
+    'fin': TokenType.FIN,
+    'libext': TokenType.LIBEXT,
+    'or': TokenType.OR,
+    'ref': TokenType.REF,
+    'si': TokenType.si,
+    'variables': TokenType.VARIABLES,
+
+    # Data types
+    'numerico': TokenType.NUMERICO,
+    'tk_numero': TokenType.TK_NUMERO,
+    'logico': TokenType.LOGICO,
+    'tk_logico': TokenType.TK_LOGICO,
+    'cadena': TokenType.CADENA,
+    'tk_cadena': TokenType.TK_CADENA,
+    'true': TokenType.TRUE,
+    'false': TokenType.TRUE,
+    'SI': TokenType.SI,
+    'NO': TokenType.NO,
+
+    # Functions
+    'leer': TokenType.LEER,
+    'imprimir': TokenType.IMPRIMIR,
+    'dim': TokenType.DIM,
+    'cls': TokenType.CLS,
+    'set_ifs': TokenType.SET_IFS,
+    'abs': TokenType.ABS,
+    'arctan': TokenType.ARCTAN,
+    'ascii': TokenType.ASCII,
+    'cos': TokenType.COS,
+    'dec': TokenType.DEC,
+    'eof': TokenType.EOF,
+    'exp': TokenType.EXP,
+    'get_ifs': TokenType.GET_IFS,
+    'inc': TokenType.INC,
+    'int': TokenType.INT,
+    'log': TokenType.LOG,
+    'lower': TokenType.LOWER,
+    'mem': TokenType.MEM,
+    'ord': TokenType.ORD,
+    'paramval': TokenType.PARAMVAL,
+    'pcount': TokenType.PCOUNT,
+    'pos': TokenType.POS,
+    'random': TokenType.RANDOM,
+    'sec': TokenType.SEC,
+    'set_stdin': TokenType.SET_STDIN,
+    'set_stdout': TokenType.SET_STDOUT,
+    'sin': TokenType.SIN,
+    'sqrt': TokenType.SQRT,
+    'str': TokenType.STR,
+    'strdup': TokenType.STRDUP,
+    'strlen': TokenType.STRLEN,
+    'substr': TokenType.SUBSTR,
+    'tan': TokenType.TAN,
+    'upper': TokenType.UPPER,
+    'val': TokenType.VAL,
+    'alen': TokenType.ALEN,
+
+  }
+
+  return keywords.get(literal, TokenType.ID)
