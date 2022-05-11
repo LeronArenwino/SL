7100885783d4d4d89f136f91f048854eefe66e7f
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-05-11 02:29:33 -0500

    :fire: Remove useless code

--- main.py
+++ main.py
@@ -1,46 +1,7 @@
-from lexer import Lexer
-from token import Token
-from typing import (
-    List,
-    Tuple
-)
-from re import (
-    match,
-    search,
-)
+from repl import start_repl
 
-comment: bool = False
-sources = []
-check_status: bool = True
-eof_if: bool = True
-pos_comment: bool = False
-right_list: List[Tuple] = []
-comment_list: List[Tuple] = []
+def main() -> None:
+    start_repl()
 
-while check_status and eof_if:
-    try:
-        line = input()
-        if bool(search(r'^\!$', line)):
-            eof_if = False
-    except EOFError:
-        break
-    sources.append(line)
-
-for line, source in enumerate(sources):
-
-    tokens: List[Token] = []
-    lexer: Lexer = Lexer(source, line + 1)
-
-    for i in range(len(source)):
-        token = lexer.next_token()
-        if token != None:
-            if check_status:
-                tokens.append(token)
-                if token.token_type.name == 'TK_ILEGAL':
-                    check_status = False
-                    break
-
-    # comment = getattr(lexer, '_comment') 
-
-    for token in tokens:
-        print(token)
+if __name__ == '__main__':
+    main()
