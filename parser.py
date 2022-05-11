d21b7b27c628ab39e63731d1c0744e712990d092
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-05-11 12:24:22 -0500

    :sparkles: New parser.py class

---
+++ parser.py
@@ -0,0 +1,9 @@
+from grammar import Grammar
+
+class Parser:
+
+    def __init__(self, grammar: Grammar) -> None:
+        self._grammar: Grammar = grammar
+
+    def see_grammar(self) -> None:
+        print(self._grammar)
