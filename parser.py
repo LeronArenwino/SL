d6cdb49c76df4340f6b585bdc88b1f217045e6a0
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-05-14 15:14:39 -0500

    :zap: first method was modified

--- parser.py
+++ parser.py
@@ -6,28 +6,42 @@ class Parser:
     def __init__(self, grammar: Grammar) -> None:
         self._grammar: Grammar = grammar
 
-    def first(self, not_terminal: str) -> Tuple:
+    def first(self, not_terminal: str) -> List:
 
+        count_productions: int = 0
         firsts_of_not_terminal: List = []
 
         not_terminal_productions = self._grammar.lookup_production(not_terminal)
 
+        """Valida si las reglas están en una lista."""
         if isinstance(not_terminal_productions, List):
 
-            state = not_terminal_productions[0]
+            production_list: List = []
+            state = not_terminal_productions[count_productions]
 
             if state in self._grammar._terminal:
 
-                firsts_of_not_terminal.append(state)
+                production_list.append(state)
+                firsts_of_not_terminal.append(production_list)
 
             elif state in self._grammar._not_terminal:
-
+                
                 firsts_of_next_not_termianl = self.first(state)
-                firsts_of_not_terminal.extend(firsts_of_next_not_termianl)
+                reduce_list = [item for sublist in firsts_of_next_not_termianl for item in sublist]
+
+                print(reduce_list)
+                if '&' in reduce_list:
+
+                    firsts_of_next_not_termianl = self.first(state)
+                    print(firsts_of_next_not_termianl)
+
+                firsts_of_not_terminal.extend(reduce_list)
 
             else:
                 
-                firsts_of_not_terminal.append('&')
+                firsts_of_not_terminal.append(['&'])
+
+        
 
         elif isinstance(not_terminal_productions, Tuple):
 
@@ -37,7 +51,7 @@ class Parser:
 
                 if isinstance(not_terminal_production, List):
 
-                    state = not_terminal_production[0]
+                    state = not_terminal_production[count_productions]
 
                     if state in self._grammar._terminal:
 
@@ -48,17 +62,68 @@ class Parser:
 
                         firsts_of_next_not_termianl = self.first(state)
                         reduce_list = [item for sublist in firsts_of_next_not_termianl for item in sublist]
-                        firsts_of_not_terminal.append(reduce_list)
+                        print(reduce_list)
+
+                        if '&' in reduce_list:
+                            
+                            if count_productions >= len(not_terminal_production):
+
+                                firsts_of_not_terminal.append(reduce_list)
+
+                            else:
+                                
+                                for production in reduce_list:
+                                    
+                                    print(production)
+                                    next_productions: List = []
+
+                                    if '&' != production:
+
+                                        next_productions.append(production)
+                                        print(next_productions)
+                                        firsts_of_not_terminal.append(next_productions)
+
+                                    else:
+
+                                        state = not_terminal_production[count_productions + 1]
+                                        print(state)
+                                        firsts_of_next_not_termianl = self.first(state)
+
+                                        if len(firsts_of_next_not_termianl) > 0:
+                                            
+                                            if '&' in firsts_of_next_not_termianl:
+
+                                                firsts_of_next_not_termianl = self.first(state)
+
+                                            print('aca')
+                                            print(firsts_of_next_not_termianl)
+                                            reduce_list = [item for sublist in firsts_of_next_not_termianl for item in sublist]
+                                            firsts_of_not_terminal.append(reduce_list)
+
+                                        else:
+                                            print('aca')
+                                            firsts_of_not_terminal.append(reduce_list)
+                                            count_productions += 1
+
+                        else:
+
+                            firsts_of_not_terminal.append(reduce_list)
                     
                     else:
                 
-                        firsts_of_not_terminal.append('&')
+                        firsts_of_not_terminal.append(['&'])
 
         else:
 
-            firsts_of_not_terminal.append('&')
+            firsts_of_not_terminal.append(['&'])
         
-        return tuple(firsts_of_not_terminal)
+        return firsts_of_not_terminal
+
+    def _peek_token(self) -> str:
+        if self._read_position >= len(self._source):
+            return ''
+        
+        return self._source[self._read_position]
 
     def see_grammar(self) -> None:
         print(self._grammar)
