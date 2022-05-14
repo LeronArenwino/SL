57626566368489aa890fe7e6a47d16636b408543
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-05-14 01:46:09 -0500

    :zap: first method was modified

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from grammar import Grammar
 
 class Parser:
@@ -6,16 +6,54 @@ class Parser:
     def __init__(self, grammar: Grammar) -> None:
         self._grammar: Grammar = grammar
 
-    def first(self, not_terminal: str) -> None:
-        print(self._grammar.lookup_production(not_terminal))
+    def first(self, not_terminal: str) -> List:
+
+        firsts_of_not_terminal: List = []
+
         not_terminal_productions = self._grammar.lookup_production(not_terminal)
 
         if isinstance(not_terminal_productions, List):
-            print(not_terminal_productions[0])
-        else:
+
+            state = not_terminal_productions[0]
+
+            if state in self._grammar._terminal:
+
+                firsts_of_not_terminal.append(state)
+
+            elif state in self._grammar._not_terminal:
+
+                firsts_of_next_not_termianl = self.first(state)
+                firsts_of_not_terminal.extend(firsts_of_next_not_termianl)
+
+            else:
+                
+                firsts_of_not_terminal.append('&')
+
+        elif isinstance(not_terminal_productions, Tuple):
+
             for not_terminal_production in not_terminal_productions:
                 if isinstance(not_terminal_production, List):
-                    print(not_terminal_production[0])  
+
+                    state = not_terminal_production[0]
+
+                    if state in self._grammar._terminal:
+
+                        firsts_of_not_terminal.append(state)
+
+                    elif state in self._grammar._not_terminal:
+
+                        firsts_of_next_not_termianl = self.first(state)
+                        firsts_of_not_terminal.extend(firsts_of_next_not_termianl)
+                    
+                    else:
+                
+                        firsts_of_not_terminal.append('&')
+
+        else:
+
+            firsts_of_not_terminal.append('&')
+        
+        return firsts_of_not_terminal
 
     def see_grammar(self) -> None:
         print(self._grammar)
