304ca8f382965984d7667bfa6182e04137bcb39a
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-05-14 11:31:15 -0500

    :zap: first method was modified

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ class Parser:
     def __init__(self, grammar: Grammar) -> None:
         self._grammar: Grammar = grammar
 
-    def first(self, not_terminal: str) -> List:
+    def first(self, not_terminal: str) -> Tuple:
 
         firsts_of_not_terminal: List = []
 
@@ -32,18 +32,23 @@ class Parser:
         elif isinstance(not_terminal_productions, Tuple):
 
             for not_terminal_production in not_terminal_productions:
+
+                production_list: List = []
+
                 if isinstance(not_terminal_production, List):
 
                     state = not_terminal_production[0]
 
                     if state in self._grammar._terminal:
 
-                        firsts_of_not_terminal.append(state)
+                        production_list.append(state)
+                        firsts_of_not_terminal.append(production_list)
 
                     elif state in self._grammar._not_terminal:
 
                         firsts_of_next_not_termianl = self.first(state)
-                        firsts_of_not_terminal.extend(firsts_of_next_not_termianl)
+                        reduce_list = [item for sublist in firsts_of_next_not_termianl for item in sublist]
+                        firsts_of_not_terminal.append(reduce_list)
                     
                     else:
                 
@@ -53,7 +58,7 @@ class Parser:
 
             firsts_of_not_terminal.append('&')
         
-        return firsts_of_not_terminal
+        return tuple(firsts_of_not_terminal)
 
     def see_grammar(self) -> None:
         print(self._grammar)
